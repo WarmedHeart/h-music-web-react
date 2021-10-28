3e488747244daa640e313cc432c10a08dd67043f
thang <16thguo@gmail.com>
2021-10-28 21:40:16 +0800

    大概布局划分

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { memo } from 'react'
 
+import HAppHeader from '@/components/app-header';
+import HAppFooter from '@/components/app-footer';
+
 export default memo(function App() {
   return (
     <div>
-      <h2>App</h2>
+      <HAppHeader />
+      main
+      <HAppFooter />
     </div>
   )
 })
