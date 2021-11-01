b9e47536bfe1a2eb13818b67f88b71a381cfe3c8
thang <16thguo@gmail.com>
2021-11-01 22:15:01 +0800

    基本引入redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { memo } from 'react';
+import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
+import { HashRouter } from 'react-router-dom';
 
 import routes from './router';
+import store from './store';
 
 import HAppHeader from '@/components/app-header';
 import HAppFooter from '@/components/app-footer';
-import { HashRouter } from 'react-router-dom';
+
 
 export default memo(function App() {
   return (
-    <HashRouter>
-      <HAppHeader />
-      {renderRoutes(routes)}
-      <HAppFooter />
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <HAppHeader />
+        {renderRoutes(routes)}
+        <HAppFooter />
+      </HashRouter>
+    </Provider>
   )
 })
