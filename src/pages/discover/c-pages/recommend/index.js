4177ba4d1275d2bb247668f3b1c36b1a5f3ef023
thang <16thguo@gmail.com>
2021-11-04 20:39:56 +0800

    使用immutable获取语法糖getIn

--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -7,7 +7,8 @@ import { getTopBannerAction } from "./store/actionCreater";
 function HRecommend(props) {
   // 组件和redux关联获取数据和操作数据（借助react-redux中的hooks）
   const { topBanners } = useSelector(state => ({
-    topBanners: state.get("recommend").get("topBanners")
+    // topBanners: state.get("recommend").get("topBanners")
+    topBanners: state.getIn(["recommend", "topBanners"])
   }), shallowEqual);  // 默认===比较返回值，修改shallowEqual为浅层比较
 
   const dispatch = useDispatch();
