98bfe6cded73d6a5d29d3395d24a91353d0e4f49
thang <16thguo@gmail.com>
2021-11-04 20:30:36 +0800

    项目中现有的reducer中对象改为immutable对象

--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -7,7 +7,7 @@ import { getTopBannerAction } from "./store/actionCreater";
 function HRecommend(props) {
   // 组件和redux关联获取数据和操作数据（借助react-redux中的hooks）
   const { topBanners } = useSelector(state => ({
-    topBanners: state.recommend.topBanners
+    topBanners: state.recommend.get("topBanners")
   }), shallowEqual);  // 默认===比较返回值，修改shallowEqual为浅层比较
 
   const dispatch = useDispatch();
