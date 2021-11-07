ac94649e5f3d9d07801fe8a7a40b15da1a3252a9
thang <16thguo@gmail.com>
2021-11-07 17:18:12 +0800

    封装推荐导航组件

---
+++ src/components/theme-header-recommend/index.js
@@ -0,0 +1,44 @@
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+
+import { HeaderWrapper } from './style';
+
+const HThemeHeaderRecommend = memo(function(props) {
+  const { title, keywords } = props;
+  
+  return (
+    <HeaderWrapper className="sprite_02">
+      <div className="left">
+        <h2 className="title">{title}</h2>
+        <div className="keyword">
+          {
+            keywords.map((item, index) => {
+              return (
+                <div className="item" key="item">
+                  <a href="todo">{item}</a>
+                  <span className="divider">{ index !== keywords.length - 1 ? '|' : ''} </span>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+      <div className="right">
+        <a href="todo">更多</a>
+        <i className="icon sprite_02"></i>
+      </div>
+    </HeaderWrapper>
+  )
+});
+
+HThemeHeaderRecommend.propTypes = {
+  title: PropTypes.string.isRequired,
+  keywords: PropTypes.array
+};
+
+HThemeHeaderRecommend.defaultProps = {
+  keywords: []
+};
+
+export default HThemeHeaderRecommend;
+
