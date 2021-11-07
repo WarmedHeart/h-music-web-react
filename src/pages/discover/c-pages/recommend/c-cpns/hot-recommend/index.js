bb2f11f2d6b07b58a71d5a59dec47d982b9448ed
thang <16thguo@gmail.com>
2021-11-07 18:19:25 +0800

    热门推荐-完成歌单组件

--- src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
@@ -4,6 +4,7 @@ import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { HOT_RECOMMEND_LIMIT } from '@/common/contants';
 
 import HThemeHeaderRecommend from '@/components/theme-header-recommend';
+import HSongsCover from '@/components/songs-cover';
 
 import { HotRecommendWrapper } from './style';
 import { getHotBannerAction } from '../../store/actionCreater';
@@ -29,11 +30,13 @@ export default memo(function HHotRecommend() {
     <HotRecommendWrapper>
       <HThemeHeaderRecommend title="热门推荐" keywords={["华语", "流行", "民谣", "摇滚", "电子"]} />
       {
-        hotRecommends.map((item, index) => (
-          <div key={item.id}>
-            {item.name}
-          </div>
-        ))
+        <div className="recommend-list ">
+        {
+          hotRecommends.map((item, index) => (
+            <HSongsCover key={item.id} info={item} />
+          ))
+        }
+        </div>
       }
     </HotRecommendWrapper>
   )
